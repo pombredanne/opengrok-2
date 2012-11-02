20f35b4f17c04e0b6bef475e51d93957d3ae6cf5
Peter M. Petrakis <peter.petrakis@canonical.com>
2012-11-02 16:39:42 -0400

    * testing new JSON nested array for improved opengrok configuration - basic multiple project support * config-changed is half-way through python migration - parsing JSON in bash with nested arrays is just too much * config_changed successfuly parses JSON provided by config.yaml - For whatever reason StringIO could not be used for intermediary, using tempfiles * introduced 'install_debug_helper', local vimrc that enables modelines

--- hooks/opengrok_common.py
+++ hooks/opengrok_common.py
@@ -1,56 +1,38 @@
-#!/usr/bin/env python3
-# -*- coding: utf=8 -*-
+#!/usr/bin/env python
 # vim:ts=2:sw=2:et:
 
-import os, sys
+from __future__ import print_function
+import os, sys, json
 from os.path import dirname, basename, abspath
-from subprocess import Popen
+from subprocess import Popen, PIPE
+from tempfile import TemporaryFile as mkstemp
 
-# XXX must import variables set in inc/common, export grok settings
-CHARM_ROOT = os.environ['PWD']
 COMMAND = basename(abspath(__file__))
 
 def juju_log(args):
     Popen(['juju-log', args], shell=False)
 
 def configure_opengrok():
-    og_content = Popen("config-get og-content", shell=True).strip()
+    print ('entering config-changed')
 
-    if og_content.startswith('lp:'):
-        repo_name = og.content.split(':')[-1]
-    else:
-        juju_log("No lp branch provided, failing safe")
-        return 0
+    scratch = mkstemp(mode='rw+b')
+    task = Popen("config-get og-content", stdout=scratch, shell=True)
+    task.communicate()
 
-    # Handle slashified urls
-    (head, tail) = os.path.split(repo_name)
+    # must rewind before we read again 
+    scratch.seek(0) 
+    og_content = json.load(scratch)
+    scratch.close()
 
-    # We define some naming policy here, to safe guard against paths
-    # like lp:foo/bar/project/trunk, we take name like this and make
-    # a new project name of project_trunk. Yes I know this leaves things
-    # out like release but it's either this or a runtime config alias
-    # that I can't always count on people providing. We may eventually
-    # get there but in the meanwhile lets do something sensible.
-    if head is not None and tail is not None:
-        (new_head, new_tail) = os.path.split(head)
-        repo_name = new_head + '_' + tail
-    else:
-        repo_name = tail
+    for repo in og_content['repos']:
+      out = "1:{0} 2:{1}".format(repo['url'], repo['alias'])
+      juju_log(out)
+      print (out)
 
-    repo_path = os.path.join(grok_src, repo_name)
-
-    if os.path.exists(repo_path):
-        juju_log("Updating existing branch - {0}".format(og_content))
-    else
-        juju_log("Checking out new branch - {0}".format(og_content))
-        result = Popen("bzr branch {0} {1}".format(og_content, repo_path),
-                shell=True)
-        # XXX check return value, DO NOT FAIL if non zero.
-        # TODO now what?
-    
-    # update config and update index
-    Popen("initctl emit --no-wait opengrok-index", shell=True)
     return 0
+    # update config and update index
+#    Popen("initctl emit --no-wait opengrok-index", shell=True)
+#    return 0
 
 # There's no switch/case in python but this construct is just as good
 try:
