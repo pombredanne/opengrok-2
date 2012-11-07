5b4983d6e1f7d070ce55daadf6ecc015a003fb62
Peter M. Petrakis <peter.petrakis@canonical.com>
2012-11-07 17:11:57 -0500

    * adjust python helper to operate as subprocess of bash script to   inherit shell environment * py-config-changed can setup multiple projects now * inc/common export GROK_SRC

--- hooks/opengrok_common.py
+++ hooks/opengrok_common.py
@@ -2,7 +2,7 @@
 # vim:ts=2:sw=2:et:
 
 from __future__ import print_function
-import os, sys, json
+import os, sys, re, json
 from os.path import dirname, basename, abspath
 from subprocess import Popen, PIPE
 from tempfile import TemporaryFile as mkstemp
@@ -10,34 +10,67 @@ from tempfile import TemporaryFile as mkstemp
 COMMAND = basename(abspath(__file__))
 
 def juju_log(args):
-    Popen(['juju-log', args], shell=False)
+  Popen(['juju-log', args], shell=False)
+
+def checkout_git(url, path):
+  Popen("git clone {0} {1}".format(url, path), shell=True).communicate()
+
+def checkout_bzr(url, path):
+  Popen("bzr branch {0} {1}".format(url, path), shell=True).communicate()
+
+def update_index():
+  Popen("initctl emit --no-wait opengrok-index", shell=True).communicate()
 
 def configure_opengrok():
-    print ('entering config-changed')
+  scratch = mkstemp(mode='rw+b')
+  task = Popen("config-get og-content", stdout=scratch,
+      shell=True).communicate()
 
-    scratch = mkstemp(mode='rw+b')
-    task = Popen("config-get og-content", stdout=scratch, shell=True)
-    task.communicate()
+  scratch.seek(0) # must rewind before we read again
+  og_content = json.load(scratch)
 
-    # must rewind before we read again 
-    scratch.seek(0) 
-    og_content = json.load(scratch)
-    scratch.close()
+  for repo in og_content['repos']:
+    url_proto=None
 
-    for repo in og_content['repos']:
-      out = "1:{0} 2:{1}".format(repo['url'], repo['alias'])
-      juju_log(out)
-      print (out)
+    juju_log("XXX {0} -- {1}".format(repo['url'], repo['alias']))
+    # XXX can't just silently ignore stuff
+    if repo['url'] is None:
+      continue
 
-    return 0
-    # update config and update index
-#    Popen("initctl emit --no-wait opengrok-index", shell=True)
-#    return 0
+    if repo['alias'] is None:
+      # XXX slashifying is just too much work, provide an alias or FAIL
+      # XXX create exception and exit(1)
+      continue
 
-# There's no switch/case in python but this construct is just as good
+    if re.match("^(lp:|lp:\~|bzr:\/\/|bzr+ssh:\/\/)", repo['url']) != None:
+      juju_log("matched a bzr url %{0}".format(repo['url']))
+      url_proto='bzr'
+
+    if re.match("(^git@|^git:\/\/|\.git$)", repo['url']) != None:
+      juju_log("matched a git url %{0}".format(repo['url']))
+      url_proto='git'
+
+    try:
+      project = abspath(os.path.join(os.environ['GROK_SRC'], repo['alias']))
+      
+      if os.path.exists(project):
+        # consider always overwrite flag
+        juju_log("Skipping project {0}, directory exists".format(project))
+        continue
+
+      try:
+        {'git' : checkout_git,
+         'bzr' : checkout_bzr} [url_proto](repo['url'], project)
+      except KeyError:
+        juju_log("repo protocol {0} currently unsupported".format(url_proto))
+
+    except KeyError:
+      juju_log("env key not found, was inc/common loaded?")
+
+  return 0
 try:
-    {'config-changed': configure_opengrok}[COMMAND]()
+  {'py-config-changed': configure_opengrok}[COMMAND]()
 except KeyError:
-    juju_log("Command not supported")
+  juju_log("Command not supported")
 
 sys.exit(0)
