fc712e2ce70bdfd5be8beb49577fc9e6310c3a7c
Peter M. Petrakis <peter.petrakis@canonical.com>
2012-11-16 17:05:30 -0500

    Revisions based on charm store submission feedback
    * README updated with current config file JSON style
    * aliases are mandatory, documentation updated to reflect this
    * config.yaml has single project default with syntax example for multiple projects
    * py-config-changed
    - reworked to __main__ invocation, otherwise unable to enter via pdb
    - smarter key/value testing, robust error reporting
    - actually use exceptions and let main decide exit status
    * added example deploy.yaml

--- hooks/opengrok_common.py
+++ hooks/opengrok_common.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import os, sys, re, json
 from os.path import dirname, basename, abspath
 import subprocess
-from subprocess import Popen, PIPE, check_output
+from subprocess import Popen, PIPE, STDOUT, check_output
 from cStringIO import StringIO
 
 COMMAND = basename(abspath(__file__))
@@ -30,52 +30,65 @@ def configure_opengrok():
   og_content = json.load(scratch)
 
   for repo in og_content['repos']:
-    url_proto=None
-
-    juju_log("XXX {0} -- {1}".format(repo['url'], repo['alias']))
-    # XXX can't just silently ignore stuff
-    if repo['url'] is None:
-      continue
-
-    if repo['alias'] is None:
-      # XXX slashifying is just too much work, provide an alias or FAIL
-      # XXX create exception and exit(1)
-      continue
-
-    if re.match("^(lp:|lp:\~|bzr:\/\/|bzr+ssh:\/\/)", repo['url']) != None:
-      juju_log("matched a bzr url %{0}".format(repo['url']))
-      url_proto='bzr'
-
-    if re.match("(^git@|^git:\/\/|\.git$)", repo['url']) != None:
-      juju_log("matched a git url %{0}".format(repo['url']))
-      url_proto='git'
+    url_proto = None
+    url = None
+    alias = None
 
     try:
-      cmd = [ 'config-get', 'grok_src' ]
-      grok_src = check_output(cmd).strip()
-      project = abspath(os.path.join(grok_src, repo['alias']))
-      
-      if os.path.exists(project):
-        # consider always overwrite flag
-        juju_log("Skipping project {0}, directory exists".format(project))
-        continue
-
-      try:
-        {'git' : checkout_git,
-         'bzr' : checkout_bzr} [url_proto](repo['url'], project)
-      except KeyError:
-        juju_log("repo protocol {0} currently unsupported".format(url_proto))
-        sys.exit(1)
-
+      if repo['url'] is None:
+        raise ValueError
+    except ValueError:
+      juju_log("Broken configuration file, url value is missing!")
+      raise
     except KeyError:
-      juju_log("env key not found, was inc/common loaded?")
+      juju_log("Broken configuration file, url key is missing!")
+      raise
+    else:
+      url = repo['url']
+
+    try:
+      if repo['alias'] is None:
+        raise ValueError
+    except ValueError:
+      juju_log("Broken configuration file, alias value is missing!")
+      raise
+    except KeyError:
+      juju_log("Broken configuration file, alias key is missing!")
+      raise
+    else:
+      alias = repo['alias']
+
+    if re.match("^(lp:|lp:\~|bzr:\/\/|bzr+ssh:\/\/)", url) != None:
+      juju_log("matched a bzr url %{0}".format(url))
+      url_proto='bzr'
+
+    if re.match("(^git@|^git:\/\/|\.git$)", url) != None:
+      juju_log("matched a git url %{0}".format(url))
+      url_proto='git'
+
+    cmd = [ 'config-get', 'grok_src' ]
+    grok_src = check_output(cmd).strip()
+    project = abspath(os.path.join(grok_src, alias))
+    
+    if os.path.exists(project):
+      juju_log("Skipping project {0}, directory exists".format(project))
+      continue
+
+    try:
+      {'git' : checkout_git,
+       'bzr' : checkout_bzr} [url_proto](url, project)
+    except KeyError:
+      juju_log("repo protocol {0} currently unsupported".format(url_proto))
+      raise
 
   update_index()
 
   return 0
-try:
-  {'py-config-changed': configure_opengrok}[COMMAND]()
-except KeyError:
-  juju_log("Command not supported")
 
-sys.exit(0)
+if __name__ == "__main__":
+  try:
+    configure_opengrok()
+  except Exception:
+    sys.exit(1)
+
+  sys.exit(0)
